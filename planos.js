c5cdb2ff3901f4a13a8b29996a8d090fea2478b0
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2023-11-25 08:11:05 -0300

    25planos

--- planos.js
+++ planos.js
@@ -1,17 +1,38 @@
 const express = require('express');
-const app = express();
 const mysql = require('mysql2');
-const port = 3003;
+const ejs = require('ejs');
+const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
-
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'phpmyadmin',
   password: 'aluno',
   database: 'CCWAmedicine',
 });
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro de conexão: ' + err.stack);
+    return;
+  }
+  console.log('Conectado como ID ' + connection.threadId);
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/planos', (req, res) => {
+  connection.query('SELECT * FROM planos', (err, rows) => {
+    if (err) throw err;
+    res.render('planos', { planos: rows });
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
+
+
+
 
 
 
